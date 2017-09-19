709b87eaea2b46d6d7636383348787f63b3b0560
jungho <jungting20@gmail.com>
2017-09-20 01:09:56 +0900

    밸리데이션 에러

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'eu-kr'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.forms import ValidationError
+import re
 # Create your models here.
+
+def lnglat_validator(value):
+    if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)$',value):
+        raise ValidationError("Invalid LngLat Type")
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,choices=(
+        ('제목1','제목1 레이블'),
+        ('제목2','제목2 레이블'),
+        ('제목3','제목3 레이블'),
+    )) #html select 를 만들어줌 choices 옵션의 힘
+    content = models.TextField()
+    lnglat = models.CharField(max_length=50,blank=True,validators=[lnglat_validator])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
