91bfb10edcae1eebe6f439d2267760585c02e859
jungho <jungting20@gmail.com>
2017-09-23 16:26:00 +0900

    admin 공부

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+#단 한번만 등록 언레지스터도있음
+#등록법 1
+#기본모든 컬럼을 줌
+
+#admin.site.register(Post)
+
+#등록법2 커스터마이징하는법
+#admin.ModelAdmin 상속받으면됨
+#이거하면 진짜 게시판처럼나옴 admin 사이트페이지가 ㅋㅋㅋ
+#이게좀더 세련됨!
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','title','created_at','updated_at']
+#admin.site.register(Post,PostAdmin)
+#이렇게도하고
+
--- blog/models.py
+++ blog/models.py
@@ -2,19 +2,25 @@ from django.db import models
 from django.forms import ValidationError
 import re
 # Create your models here.
+from django.utils.datetime_safe import datetime
+
 
 def lnglat_validator(value):
     if not re.match(r'(\d+\.?\d*),(\d+\.?\d*)$',value):
         raise ValidationError("Invalid LngLat Type")
 
+#프로젝트를 만들자마자 migrate 하면 어드민과 유저모델이 디폴트로 생성
+#migrate appname zero 이렇게 하게되면 모든 마이그레이션을 취소함!!
+#showmigrations blog 로 적용 미적용 알수있다
+#블랭크옵션,널옵션을 주지않으면 이건 필수필드이므로 디폴트값을 정해줘야함
+#마이그레이트를 수행하면 디폴트값 정해달라고 나옴 이미 들어있는 값들에 넣어줄 값들
+#그걸 말하는 거지
 
 class Post(models.Model):
-    title = models.CharField(max_length=100,choices=(
-        ('제목1','제목1 레이블'),
-        ('제목2','제목2 레이블'),
-        ('제목3','제목3 레이블'),
-    )) #html select 를 만들어줌 choices 옵션의 힘
-    content = models.TextField()
+    authon = models.CharField(max_length=20,default='test')
+    title = models.CharField(max_length=100
+    ) #html select 를 만들어줌 choices 옵션의 힘
+    content = models.TextField(blank=True,null=True)
     lnglat = models.CharField(max_length=50,blank=True,validators=[lnglat_validator])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
